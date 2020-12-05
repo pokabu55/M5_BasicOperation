9569c48abb8d6c16eb1c267a35c036ff9c41c0a6
pokabu55 <okazumaru@gmail.com>
2020-12-05 17:57:35 +0900

    HelloWorldの表示

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
+#include <M5Stack.h>
 
 void setup() {
-  // put your setup code here, to run once:
+	// put your setup code here, to run once:
+
+	// M5Stackの初期化
+	M5.begin();
+
+	// ディスプレイに表示
+	M5.Lcd.print("Hello world!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	// put your main code here, to run repeatedly:
 }
