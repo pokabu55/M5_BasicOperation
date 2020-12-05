94f6a3eb56365512f8fe45741db9f638ac1cb4b2
pokabu55 <okazumaru@gmail.com>
2020-12-05 18:31:57 +0900

    図形表示とシリアル出力

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,19 @@ void setup() {
 	// M5Stackの初期化
 	M5.begin();
 
+	// LCDの明るさを変更
+	M5.Lcd.setBrightness(128);
+
 	// ディスプレイに表示
 	M5.Lcd.print("Hello world!");
+
+	M5.Lcd.fillRect(100,100,50,60,TFT_GREEN);
+
+	// シリアルを使った print デバッグ
+	Serial.println("done.");
+
+	// 色、位置を指定した文字表示
+	showText();
 }
 
 void loop() {
