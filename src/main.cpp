e157b244121395bafbd89ad90ec66f957c6d6158
pokabu55 <okazumaru@gmail.com>
2020-12-13 20:47:20 +0900

    main関数から、不要なコードを削除

--- src/main.cpp
+++ src/main.cpp
@@ -41,32 +41,9 @@ void showText2()
 	// フォントサイズを決める
 	uint8_t fontSize = 2;
 	M5.Lcd.setTextSize(fontSize);
-
-	// 色の指定
-	// M5Stackは、Rが5bit、Gが6ビット、Bが5ビットの16ビットなんだって
-	// なので、普通のRGB それぞれが8ビットで考えるきの変換式は
-	uint8_t R = 255;
-	uint8_t G = 255;
-	uint8_t B = 255;
-	uint16_t RGB565 = ((R>>3)<<11) || ((G>>2)<<5) || ((B>>3));
 	
 	Serial1.printf("%d",RGB565);
 
-	// 文字の色を設定
-	M5.Lcd.setTextColor(RGB565);
-	M5.Lcd.setTextColor(TFT_GREEN);
-
-	// 出力位置
-	int16_t x = 20, y = 20;
-	M5.Lcd.setCursor(x,y);
-
-	// おりかえしをON、デフォルトONなのか？
-	// 折返しがないと、右へ書き続けて表示できなくなる
-	M5.Lcd.setTextWrap(true);
-
-	// 文字列
-	M5.Lcd.println("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-	M5.Lcd.println("012345678901234567890123456789012345678901234567890123456789");
 }
 
 void showImage()
@@ -86,67 +63,21 @@ void setup() {
 	// シリアルを使った print デバッグ
 	Serial.println("done.");
 
-	// 色、位置を指定した文字表示
-	//showText();
-	//showText2();
-
-	// 画像を表示
-	//showImage();
-
 	// メッセージ
 	M5.Lcd.drawCentreString("Press any key.", 160, 120, 4);
-
-	// 乱数の初期化
-	//randomSeed(analogRead(0));
 }
 
 void loop() {
 	// put your main code here, to run repeatedly:
 
-	/*long x = random(0, DISPLAY_WIDTH);
-	long y = random(0, DISPLAY_HEIGHT);
-	long r = random(0, 200);*/
-
 	if (M5.BtnA.wasPressed()) {
 		// ボタンAを押したときの振る舞い
-		//M5.Lcd.drawRect(x-r, y-r, x+r, y+r, TFT_BLUE);
-
-		// 画像送り機能
-		imageShow.moveNextImage();
-
-		// ボタンを押したら画像を表示
-		imageShow.show();
-
 	}
 	if (M5.BtnB.wasPressed()) {
 		// ボタンBを押したときの振る舞い
-		//M5.Lcd.drawCircle(x, y, r, TFT_RED);
-
-		Serial.println("BtnB Pressed-1.");
-		// 明るくする
-		imageShow.brighten();
-
-		Serial.println("BtnB Pressed-2.");
-		Serial.printf("ratio = %d\n", imageShow.brightnessRatio_);
-
-		// ボタンを押したら画像を表示
-		imageShow.show();
-
-		Serial.println("BtnB Pressed-3.");
 	}
 	if (M5.BtnC.wasPressed()) {
 		// ボタンCを押したときの振る舞い
-		//M5.Lcd.fillScreen(BLACK);
-
-		Serial.println("BtnC Pressed.");
-
-		// 暗くする
-		imageShow.darken();
-
-		Serial1.printf("ratio = %d\n", imageShow.brightnessRatio_);
-
-		// ボタンを押したら画像を表示
-		imageShow.show();
 	}
 
 	// ボタンが押されたかを確認するため
